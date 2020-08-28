8b366e50e5436d77124da8f2b8db24290f9dafcb
João Pedro Harbs <harbsprog@gmail.com>
2020-08-28 20:07:32 -0300

    alterando ip

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: `http://192.168.0.23:8080/api`,
-  // baseURL: `http://177.85.33.222:8081/api`,
+  // baseURL: `http://192.168.0.23:8080/api`,
+  baseURL: `http://177.85.33.222:8081/api`,
 })
